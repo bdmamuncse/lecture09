e8aed4cb8c6531c3131b941426271610ead54fec
sazzad <bdmamuncse@gmail.com>
2022-01-04 04:10:34 +0600

    button add

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,8 +10,42 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  List<String> userName=['Rahim','Karim','Abul','user-1','user-2'];
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        actions: [
+          IconButton(
+            onPressed: (){
+              print('Before add data${userName.length}');
+              userName.add('New Name');
+              print('Before add data${userName.length}');
+
+            }, icon: const Icon(Icons.add),),
+        ],
+      ),
+      drawer: const Drawer(),
+      body: Container(
+
+        child: ListView.builder(
+            itemCount: userName.length,
+            itemBuilder: (context, index) {
+              return Container(
+                margin: EdgeInsets.only(bottom: 2),
+                height: 40,
+                width: double.infinity,
+                color: Colors.grey,
+                child: Center(child: Text(userName[index])),
+              );
+            } ,
+        ),
+        ),
+
+      );
   }
 }
