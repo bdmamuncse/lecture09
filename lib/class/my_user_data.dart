6c3416ac14440ee481070a2f9400cbc63a688ce9
Sazzad_dart_practice <bdmamunce@gmail.com>
2022-01-04 18:20:28 +0600

    lect 9 upto list add using class listing

---
+++ lib/class/my_user_data.dart
@@ -0,0 +1,12 @@
+class MyUserData{
+  String username;
+  String userphone;
+  String useremail;
+
+  MyUserData({
+    required this.username,
+    required this.userphone,
+    required this.useremail
+});
+
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lecture_09_1_fresh_start/class/my_user_data.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,7 +12,12 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
-  List<String> userName=['Rahim','Karim','Abul','user-1','user-2'];
+  List<MyUserData> userName=[
+    MyUserData(username: 'Md. Sazzad Hossain', userphone: '01712045195', useremail: 'bdmamuncse@gmail.com'),
+    MyUserData(username: 'user-1',userphone: '01712045195',useremail: 'bdmamuncse@gmail.com'),
+    MyUserData(username: 'user-2',userphone: '01712045195',useremail: 'bdmamuncse@gmail.com'),
+    MyUserData(username: 'user-3',userphone: '01712045195',useremail: 'bdmamuncse@gmail.com')
+  ];
 
 
   @override
@@ -22,11 +28,16 @@ class _HomeScreenState extends State<HomeScreen> {
         actions: [
           IconButton(
             onPressed: (){
-              print('Before add data${userName.length}');
-              userName.add('New Name');
-              print('Before add data${userName.length}');
 
-            }, icon: const Icon(Icons.add),),
+              setState(() {
+
+                print('Before add data${userName.length}');
+                userName.add(MyUserData(username:'new user', userphone: '797989808', useremail: 'mamun@gmail.com'));
+                print('After add data${userName.length}');
+
+              });
+
+            }, icon: const Icon(Icons.add)),
         ],
       ),
       drawer: const Drawer(),
@@ -35,12 +46,20 @@ class _HomeScreenState extends State<HomeScreen> {
         child: ListView.builder(
             itemCount: userName.length,
             itemBuilder: (context, index) {
-              return Container(
-                margin: EdgeInsets.only(bottom: 2),
-                height: 40,
-                width: double.infinity,
-                color: Colors.grey,
-                child: Center(child: Text(userName[index])),
+              return InkWell(
+                onTap: () {
+                  setState(() {
+                    userName.removeAt(index);
+                  });
+                  // print('${userName[index]}Positional Clicked');
+                },
+                child: Container(
+                  margin: EdgeInsets.only(bottom: 2),
+                  height: 40,
+                  width: double.infinity,
+                  color: Colors.grey,
+                  child: Center(child: Text(userName[index].username )),
+                ),
               );
             } ,
         ),
